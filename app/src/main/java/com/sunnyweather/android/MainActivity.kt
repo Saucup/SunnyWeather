fd9045069ec37c07e75d6ca05716566cf9521e31
sidonchen <sidonchen@ftnn.com>
2024-08-05 18:08:39 +0800

    寻找地址feature

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -2,10 +2,12 @@ package com.sunnyweather.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sunnyweather.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
